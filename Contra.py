70cba4380d561bfdc07e77b67e9aed59b8f5f8a6
sst <viking64rus@gmail.com>
2024-12-23 15:43:57 +0300

    update contra

--- Contra.py
+++ Contra.py
@@ -6,3 +6,14 @@ result = [lst[i:i+size] for i in range(0, len(lst), size)]
 for i in range(1, len(result), 2):
     result[i] = result[i][::-1]
 print(result)
+
+
+lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+n = len(lst)
+size = n // 3
+#разбиение списка на подсписки
+result = [lst[i:i+size] for i in range(0, n, size)]
+#переворот чётных подписков
+for i in range(1, len(result), 2):
+    result[i] = result[i][::-1]
+print(result)
